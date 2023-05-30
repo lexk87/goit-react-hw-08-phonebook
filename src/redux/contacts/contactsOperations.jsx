785a2d1ecb8164624168afb76a24ef9cdd8f45dc
Alexandr Katyshev <katyshev1@gmail.com>
2023-05-30 20:41:24 +0300

    adds contacts operations

---
+++ src/redux/contacts/contactsOperations.jsx
@@ -0,0 +1,53 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL =
+    'https://646e0a269c677e23218aed19.mockapi.io/phonebookapi/';
+
+export const fetchContacts = createAsyncThunk(
+    'contacts/fetchAll',
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get('contacts');
+            return response.data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+);
+
+export const addContact = createAsyncThunk(
+    'contacts/addContact',
+    async (contact, thunkAPI) => {
+        try {
+            const response = await axios.post('contacts', contact);
+            return response.data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+);
+
+export const editContact = createAsyncThunk(
+    'contacts/editContact',
+    async (contactId, thunkAPI) => {
+        try {
+            const response = await axios.patch(`contacts/${contactId}`);
+            return response.data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+);
+
+export const deleteContact = createAsyncThunk(
+    'contacts/deleteContact',
+    async (contactId, thunkAPI) => {
+        try {
+            const response = await axios.delete(`contacts/${contactId}`);
+            return response.data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+);
