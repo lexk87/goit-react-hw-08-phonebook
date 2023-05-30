82be3a862f0ee080d3991830eb86289d4b393107
Alexandr Katyshev <katyshev1@gmail.com>
2023-05-30 16:00:35 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <App />
     </React.StrictMode>
 );
